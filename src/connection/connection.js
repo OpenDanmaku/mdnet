16e972b608b6ca333e199387a6a403ba3df777bd
Jianfeng Zhang <swordfeng123@gmail.com>
2016-05-24 01:02:33 +0800

    update

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,30 +1,48 @@
+/**
+ * Module that handles connections
+ * @module connection
+ */
 const EventEmitter = require('events');
 let connection = {};
 let protocols = {};
 
-/*
+/**
+ * Connection class
+ * @class
+ */
 class Connection extends EventEmitter {
+    /**
+     * @constructor
+     * @param {string|array} endpoint - the descriptor of the endpoint
+     */
     constructor(endpoint) {
         super();
+        if (typeof endpoint === 'string') {
+            endpoint = endpoint.split(':');
+        }
+        if (typeof protocols[endpoint[0]] === 'undefined') {
+            throw new Error('unsupported protocol');
+        }
+        this._endpoint = endpoint.join(':');
+        let stream = protocols[endpoint[0]].connect(endpoint);
+        this._stream = stream;
+        stream.on('connect', () => this.emit('connect'));
+        stream.on('error', (err) => this.emit('error', err));
+        stream.on('close', () => this.emit('close'));
+        stream.on('data', (buf) => {
+            // todo: proceed data
+        });
+    }
+    /**
+     * Send message to the peer
+     * @param {Buffer} message - message to send
+     * @return {Promise}
+     */
+    send(message) {
+        // todo: send (packed & signed) data
     }
 }
-*/
-
-/**
- * Connect to the endpoint
- * @param {string|array} endpoint - the descriptor of the endpoint
- * @return {Promise.<Stream>}
- */
-function connect(endpoint) {
-    if (typeof endpoint === 'string') {
-        endpoint = endpoint.split(':');
-    }
-    if (typeof protocols[endpoint[0]] === 'undefined') {
-        throw new Error('unsupported protocol');
-    }
-    return protocols[endpoint[0]].connect(endpoint);
-};
 
 protocols.tcp = require('./tcp.js');
 
-module.exports = { /*Connection,*/ connect };
+module.exports = { Connection };
--- src/connection/tcp.js
+++ src/connection/tcp.js
@@ -1,5 +1,15 @@
+/**
+ * TCP Protocol
+ * @module connection/tcp
+ */
+
 const net = require('net');
 
+/**
+ * Connect to TCP endpoint
+ * @param {array} endpoint - the descriptor of the endpoint
+ * @return {Stream}
+ */
 function connect(endpoint) {
     let host = parseInt(endpoint[1]);
     let port = parseInt(endpoint[2]);
