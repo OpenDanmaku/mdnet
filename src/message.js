44907ca3bfb054da48eeeff333e21228d7070e12
Jianfeng Zhang <swordfeng123@gmail.com>
2016-05-24 22:04:00 +0800

    add message class

---
+++ src/message.js
@@ -0,0 +1,44 @@
+/**
+ * Message module
+ * @module message
+ */
+
+const _private = Symbol();
+
+/**
+ * Message class
+ * @class
+ */
+module.exports = class Message {
+    /**
+     * @constructor
+     * @param {string} type
+     */
+    constructor(type) {
+        this[_private] = {};
+        this[_private].type = type;
+    }
+    /**
+     * Output message in buffer
+     * @return {Buffer}
+     */
+    toBuffer() {
+        let messageObject = {
+            type: this[_private].type,
+            message: this
+        };
+        return new Buffer(JSON.stringify(messageObject), 'utf8');
+    }
+    /**
+     * Convert buffer to message
+     * @param {Buffer} buffer
+     * @return {Message}
+     */
+    static fromBuffer(buf) {
+        let messageObject = JSON.parse(buf);
+        let message = messageObject.message;
+        message.__proto__ = Message.prototype;
+        message[_private] = { type: messageObject.type };
+        return message;
+    }
+}
